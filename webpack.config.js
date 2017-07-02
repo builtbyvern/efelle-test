680d4845037161340ce4583b80ab48883384661c
John Briggs <vernworldwide@gmail.com>
2017-07-01 19:13:22 -0700

    bootstrap added to webpack. sass imports working. basic content of index is in

--- webpack.config.js
+++ webpack.config.js
@@ -24,27 +24,28 @@ module.exports = {
     // entry: './src/main.js', or 
     entry: {
         app: [
-            './src/main.js',
-            './src/main.scss'
+            'bootstrap-loader',
+            './src/js/main.js',
+            './src/scss/main.scss'
         ],
         vendor: ['jquery']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
+        filename: '[name].js'
     },
     module: {
         rules: [{
-                test: /\.s[ac]ss$/,
-                use: ExtractTextPlugin.extract({
-                    // use: ['css-loader', 'postcss-loader', 'sass-loader'], without image opt
-                    use: [{
-                            loader: 'css-loader',
-                        },
-                        'sass-loader'
-                    ],
-                    fallback: 'style-loader'
-                })
+          test: /\.s[ac]ss$/,
+          use: ExtractTextPlugin.extract({
+            // use: ['css-loader', 'postcss-loader', 'sass-loader'], without image opt
+            use: [{
+                loader: 'css-loader',
+              },
+              'sass-loader'
+            ],
+              fallback: 'style-loader'
+          })
             },
 
             {
@@ -78,6 +79,24 @@ module.exports = {
         new ExtractTextPlugin("[name].css"),
         new webpack.LoaderOptionsPlugin({
             minimize: inProduction
+        }),
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery",
+          "window.jQuery": "jquery",
+          Tether: "tether",
+          "window.Tether": "tether",
+          Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+          Button: "exports-loader?Button!bootstrap/js/dist/button",
+          Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+          Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+          Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+          Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+          Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+          Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+          Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+          Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+          Util: "exports-loader?Util!bootstrap/js/dist/util",
         }),
         new PurifyCSSPlugin({
             paths: glob.sync(path.join(__dirname, '*.html')),
